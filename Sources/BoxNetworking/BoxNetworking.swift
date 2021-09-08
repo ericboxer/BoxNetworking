f2c10c8685e02d9af73fe3b5c7715cc6a1e3ad9a
Eric Boxer <eboxhead@gmail.com>
2021-09-08 08:50:40 -0400

    Combined random items into base file

--- Sources/BoxNetworking/BoxNetworking.swift
+++ Sources/BoxNetworking/BoxNetworking.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  BoxNetowrking.swift
 //  
 //
 //  Created by Eric Boxer on 8/4/21.
@@ -8,17 +8,34 @@
 import Foundation
 
 
+// MARK: Enums
+public enum BNDispatchQueues:String {
+    public typealias RawValue = String
+    case UDP = "com.boxnetworking.queue.udp"
+    case TCP = "com.boxnetworking.queue.TCP"
+}
+
+
+public enum BNLogLevels:BNLogLevel {
+    case DEBUG = 0
+    case INFO = 10
+    case WARN = 20
+    case ERROR = 30
+    case FAIL = 40
+}
 
 /// BoxNetworking function returns
-public enum BoxNetworkingReturnStatus:String {
+public enum BNReturnStatus:String {
     case OK = "OK"
     case FAIL = "Failed"
     case ERROR = "Error"
     case MULTICAST_JOIN_ERROR = "Error joining multicast group"
 }
 
+// END: Enums
 
 
+// MARK:Structs
 
 // TODO: Make sure the Multicast Address is an actual Multicast
 public struct MulticastAddress {
@@ -37,3 +54,10 @@ public struct MulticastAddress {
         }
     }
 }
+
+// END: Structs
+
+// MARK: Typaliases
+public typealias BNLogLevel = Int
+
+// END: Typealiases
